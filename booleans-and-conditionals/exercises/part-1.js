2a6d47048004b6a391015ca3f22c8316315afd69
Your Name <gerardovazquez9509@gmail.com>
2024-01-13 12:31:47 -0600

    EXERCISES

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,4 +1,10 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "red blinking";
+let spaceSuitOn = true;
+let shuttleCabinReady= true;
+let crewStatus = spaceSuitOn && shuttleCabinReady;
+let computerStatusCode= 200;
+let shuttlespeed= 15000;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
--- booleans-and-conditionals/exercises/part-2.js
+++ booleans-and-conditionals/exercises/part-2.js
@@ -3,18 +3,31 @@ let spaceSuitsOn = true;
 let shuttleCabinReady = true;
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
-let shuttleSpeed = 15000;
-
+let shuttleSpeed = 17000;
+let commandOveride= true
 // 3) Write conditional expressions to satisfy the following safety rules:
 
 // a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
-
+if (crewStatus = true){
+    console.log("Crew Ready");
+}
+else {console.log("Crew Not Ready"); 
+}
 
 // b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+if (computerStatusCode === 200){
+    console.log("Please stand by. Computer is rebooting.");
+}else if (computerStatusCode === 400){
+    console.log("Success! Computer online." )
+} else console.log("ALERT")
 
 
 // c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
-
+if (shuttleSpeed > 17500){
+    console.log("ALERT:Escape Velocity Reached!");
+}else if (shuttleSpeed < 8000){
+    console.log("Alert: Cannot maintain orbit!");
+} else console.log("Stable speed");
 
 // 4) PREDICT: Do the code blocks shown in the 'predict.txt' file produce the same result?
 
