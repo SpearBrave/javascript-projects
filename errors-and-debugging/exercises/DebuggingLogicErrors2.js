542846ee162b75aceebe4dbfd5fb3094abe620dc
Your Name <gerardovazquez9509@gmail.com>
2024-01-13 17:28:43 -0600

    hw

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -16,7 +16,7 @@ if (fuelLevel >= 20000) {
    console.log('WARNING: Insufficient fuel!');
    launchReady = false;
 }
-
+//YES
 // if (crewStatus && computerStatus === 'green'){
 //    console.log('Crew & computer cleared.');
 //    launchReady = true;
