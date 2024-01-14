e9b164a426464504a40e0893f70589f7543e1603
Your Name <gerardovazquez9509@gmail.com>
2024-01-13 18:42:09 -0600

    hw

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,5 +1,5 @@
-console.log('Hello, JavaScript.');
-console.log(2001);
+
+);
 console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
