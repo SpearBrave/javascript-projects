7597a64c77a54a0637ea60d20d7ae7f39c9e5fe9
Your Name <gerardovazquez9509@gmail.com>
2024-01-22 16:34:53 -0600

    hw

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,10 @@
-// create an array variable containing the names
+	
+const input = require('readline-sync');
 
-// write a for loop that prints each name on a different line
+let num = input.question('Please enter a positive number:');
+num = Number(num);
+
+while (num <= 0) {
+   num = input.question('Invalid input. Please enter a positive number:');
+   num = Number(num);
+}
